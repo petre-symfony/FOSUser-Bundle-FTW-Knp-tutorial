c5f4e7ae5ddabe3dc955403c3fdfd4d55ece3476
petrero <petrero@gmail.com>
2017-06-06 20:21:12 +0300

    In a minute, we'll tell FOSUserBundle to use this form instead of its normal registration form. But... instead of completely replacing the default form, what I really want to do is just add one field to it. Is there a way to extend the existing form?
    Extending the Core Form
    
    Totally! And once again, the web debug toolbar can help us out. Mouse over the form icon and click that. This tells us what form is used on this page: it's called RegistrationFormType - the same as our form class!
    
    To build on top of that form, you don't actually extend it. Instead, override a method called getParent(). Inside, we'll return the class that we want to extend. At the top, add use, autocomplete RegistrationFormType from the bundle and put as BaseRegistrationFormType to avoid conflicting.
    
    Now in getParent(), we can say return BaseRegistrationFormType::class
    
    And that is it! This form will have the existing fields plus firstName.

--- src/AppBundle/Form/RegistrationFormType.php
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -3,9 +3,14 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
 
 class RegistrationFormType extends AbstractType{
   public function buildForm(FormBuilderInterface $builder, array $options) {
     $builder->add('firstName');
+  }
+  
+  public function getParent() {
+    return BaseRegistrationFormType::class;
   }
 }
