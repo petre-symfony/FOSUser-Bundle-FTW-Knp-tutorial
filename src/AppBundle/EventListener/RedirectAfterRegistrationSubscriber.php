93783f4004d931ecfe90fd23cc176c602f3bcd54
petrero <petrero@gmail.com>
2017-06-06 21:42:47 +0300

    Copy code from branch 8

---
+++ src/AppBundle/EventListener/RedirectAfterRegistrationSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+namespace AppBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+class RedirectAfterRegistrationSubscriber implements EventSubscriberInterface{
+  private $router;
+  
+  public function __construct(RouterInterface $router) {
+    $this->router = $router;
+  }
+  
+  public function onRegistrationSuccess(FormEvent $event) {
+    $url = $this->router->generate('homepage');
+    $response = new RedirectResponse($url);
+    $event->setResponse($response); 
+  }
+  
+  public static function getSubscribedEvents(){
+    
+    return [
+      FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess'   
+    ];  
+  }
+}
