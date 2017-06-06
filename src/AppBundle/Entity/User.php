ae646a63c55b582ca7fc1d74982904366cd69e49
petrero <petrero@gmail.com>
2017-06-06 18:40:33 +0300

    Copy code from branch 2

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser {
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   * @ORM\Column(type="integer") 
+   */
+  protected $id;
+  
+  public function getId(){
+    return $this->id;
+  }
+}
