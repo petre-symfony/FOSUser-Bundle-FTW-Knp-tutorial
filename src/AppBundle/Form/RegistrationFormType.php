ee31570091715c804ac01663d3b4d19f57a39892
petrero <petrero@gmail.com>
2017-06-06 20:54:13 +0300

    Removing the Username Field from the Form
    The only other thing we need to do is remove the username field from the registration form. How? Easy: in RegistrationFormType, add ->remove('username')
    
    Then, in the template, remove its form_row()
    
    And yea, that's it! Refresh! No more username! Register as aquanaut4@gmail.com and submit. Check it out in the database:
    
    php bin/console doctrine:query:sql 'SELECT * FROM user'
    There it is! The username is now equal to the email.
    
    Oh, and if you're using the profile edit page from the bundle, you'll also want to remove the username field from the ProfileFormType form. It's done in exactly the same way.

--- src/AppBundle/Form/RegistrationFormType.php
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -7,7 +7,9 @@ use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
 
 class RegistrationFormType extends AbstractType{
   public function buildForm(FormBuilderInterface $builder, array $options) {
-    $builder->add('firstName');
+    $builder
+      ->add('firstName')
+      ->remove('username');
   }
   
   public function getParent() {
